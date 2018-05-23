169ef3b4ba51df3a175c5aef4fcad13fe96c2430
Ranoga Stofael <Ralubrusto@users.noreply.github.com>
2018-05-23 09:31:42 -0300

    SEATEL - Dia 2

--- loops.py
+++ loops.py
@@ -2,6 +2,9 @@
 # while [condição]:
 # 	[executar isso enquanto condição for True]
 
+# Quando abrimos e fechamos 3 aspas criamos uma string multi-linha
+# Útil para criar textos em variáveis ou para criar comentários de várias linhas
+
 '''vetor = [1,2,'PC',3,4,]
 novo = []
 cont = 0
@@ -12,32 +15,34 @@ while cont<len(vetor):
 		cont +=1
 	else:
 		# cont +=1
-		# continue
-		break
+		break		# Palavra Break é especial pois faz sair do Loop
 print(novo) '''
 
 # vetor = ['''Ana''', 'Berto', "Chico", "Dani"]
 # # x = 5
 # # dicio = {'Nome':'Nao eh rafael', 'idade':14}
 
+# Laço 'for':
+# itera sobre os elementos de um iterável, que pode ser uma lista, tupla, dicionário, string...
+# iteráveis são, em geral, tipos que possuem mais de um elemento discernível entre eles
+
 # for nome in vetor:
-# 	msg = 'Seja bem vindo, {}'.format(nome)
-# 	print(msg)
+# 	msg = 'Seja bem vindo, {}'.format(nome)		# Formatação de Strings
+# 	print(msg)					# Método format insere variáveis em meio a strings
 
-# Formatando strings
 
-alunos = ['Hanna', 'Jhonatan', 'Jazz','Caio', 'Lukas','Caio']
+alunos = ['Hanna', 'Jhonatan', 'Jazz', 'Fulano', 'Caio', 'Lukas','Caio']
 
 # print('{0[0]} esta do lado de {0[1]}, que esta do lado de {0[2]}, que esta na frente do {1}'.format(alunos, 'Rafael'))
 # print('{2} esta do lado de {1}, que esta do lado de {0}'.format('Hanna', "Lukas", 'Jazz'))]
+# print('{} esta do lado de {}, que esta do lado de {}'.format('Hanna', "Lukas", 'Jazz'))]
 
 # print('{1} esta do lado de {2}, que esta do lado de {0}'.format(*alunos))
-# dar uma olhada em s/c/scepalas
 
 for aluno in alunos:
-	if aluno == 'Caio':
-		continue
-		print('OI')
+	if aluno == 'Fulano':
+		continue		# Palavra usada em laços 'for'
+					# continue faz o laço ignorar a iteração e partir para a próxima
 	# elif aluno=='Jhonatan': 
 	# 	break
 	else:
